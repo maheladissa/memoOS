f714afaa51a22307e49a7dfd2457ac45b6bc717e
maheladissa <maheladissanayake@gmail.com>
2021-07-18 21:57:39 +0530

    Creating the stack

--- loader.s
+++ loader.s
@@ -4,6 +4,18 @@
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+                                    
+                                    
+    KERNEL_STACK_SIZE equ 4096       ; size of stack in bytes
+    
+            
+        
+    section .bss
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel 
+        
+
 
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
@@ -12,6 +24,8 @@
         dd CHECKSUM                 ; and the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+	mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+	mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the stack (end of memory area)
     .loop:
         jmp .loop                   ; loop forever
+
