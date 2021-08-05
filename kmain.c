f33b916af526a3be3eb616c1ae206178c3872a7c
maheladissa <maheladissanayake@gmail.com>
2021-08-05 23:40:39 +0530

    Serial ports

--- kmain.c
+++ kmain.c
@@ -1,52 +1,14 @@
-#include "io.h"
-char *fb = (char*) 0x000B8000;
-
-/** fb_write_cell:
-*  Writes a character with the given foreground and background to position i
-*  in the framebuffer.
-*
-*  @param i  The location in the framebuffer
-*  @param c  The character
-*  @param fg The foreground color
-*  @param bg The background color
-*/
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-{
-  fb[i] = c;
-  fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-}
-
-
-/* The I/O ports */
-#define FB_COMMAND_PORT         0x3D4
-#define FB_DATA_PORT            0x3D5
-
-/* The I/O port commands */
-#define FB_HIGH_BYTE_COMMAND    14
-#define FB_LOW_BYTE_COMMAND     15
-
-/** fb_move_cursor:
-*  Moves the cursor of the framebuffer to the given position
-*
-*  @param pos The new position of the cursor
-*/
-void fb_move_cursor(unsigned short pos)
-{
-    outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
-    outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
-    outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
-    outb(FB_DATA_PORT,    pos & 0x00FF);
-}
-
-#define FB_GREEN      2
-#define FB_DARK_GREY  8
-#define FB_LIGHT_CYAN 3
-#define FB_BLACK      0
+#include "Headers/framebuffer.h"
+#include "Headers/serial.h"
 
 void kmain()
 {
 
     //fb_write_cell(0, 'A', FB_LIGHT_CYAN, FB_BLACK);
-    fb_move_cursor(00);
+    //fb_move_cursor(00);
+    char buffer[25] = "--- Welcome to MemoOS ---";
+    fb_write(buffer, 26);
+    serial_configure(SERIAL_COM1_BASE, 1);
+    serial_write(SERIAL_COM1_BASE, buffer, 26);
     
 }
