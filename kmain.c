e432e72ee9ff3970c01ea56497fc76eb0dd01e00
maheladissa <maheladissanayake@gmail.com>
2021-08-13 13:01:48 +0530

    Segmentation

--- kmain.c
+++ kmain.c
@@ -1,14 +1,24 @@
 #include "Headers/framebuffer.h"
 #include "Headers/serial.h"
+#include "Segments/gdt.h"
+
+/* Function to initialize */
+void init() {
+  /* Initialize segment descriptor tables */
+  init_gdt();
+
+  /* Initialize serial port */
+  serial_configure(SERIAL_COM1_BASE, 1);
+}
 
 void kmain()
 {
 
+    init();
     //fb_write_cell(0, 'A', FB_LIGHT_CYAN, FB_BLACK);
     //fb_move_cursor(00);
     char buffer[25] = "--- Welcome to MemoOS ---";
     fb_write(buffer, 26);
-    serial_configure(SERIAL_COM1_BASE, 1);
     serial_write(SERIAL_COM1_BASE, buffer, 25);
     
 }
