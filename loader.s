8ddf51580842384e175814915f6246a788e46d93
maheladissa <maheladissanayake@gmail.com>
2021-08-26 19:34:04 +0530

    Configuring GRUB and Loader

--- loader.s
+++ loader.s
@@ -1,9 +1,9 @@
     global loader                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0)
+    ALIGN_MODULES   equ 0x00000001      ; tell GRUB to align modules
+    ; calculate the checksum (all options + checksum should equal 0)
+    CHECKSUM        equ -(MAGIC_NUMBER + ALIGN_MODULES)
                                     
                                     
     KERNEL_STACK_SIZE equ 4096       ; size of stack in bytes
@@ -19,13 +19,14 @@
 
     section .text                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
-        dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
+        dd MAGIC_NUMBER                 ; write the magic number
+        dd ALIGN_MODULES                ; write the align modules instruction
+        dd CHECKSUM                     ; write the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
 	mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the stack (end of memory area)
 	
+	push ebx		     ;push ebx on the stack
 	extern kmain;
 	call kmain;
     .loop:
