d2327400240a34226757c74d09b20515aac6c7b3
maheladissa <maheladissanayake@gmail.com>
2021-09-10 21:17:58 +0530

    Configure page tables

--- Paging/paging.h
+++ Paging/paging.h
@@ -1,68 +1,240 @@
+// paging.h -- Defines the interface for and structures relating to paging.
+//             Written for JamesM's kernel development tutorials.
+
 #ifndef PAGING_H
 #define PAGING_H
 
-// paging mode : 
-// NON PAE 4K Mode (32 bit) - level 0 page, 1 PT, 2 PD  <--  
-// NON PAE 4M Mode (32 bit) - level 0 page, 2 PD 
-// PAE 4K Mode (32 bit) - level 0 page, 1 PT, 2 PD, 3 PDP 
-// PAE 4M Mode (32 bit) - level 0 page, 2 PD, 3 PDP
-// LONG MODE 4K (64 bit) - level 0 page, 1 PT, 2 PD, 3 PDP, 4 PML4
-
-
-#define NUM_PAGES 1024
-#define PAGE_FRAME_SIZE 4096
-
-#define PRESENT         1
-#define PAGE_READONLY   0
-#define PAGE_READWRITE  1
-#define PAGE_USER       1
-#define PAGE_KERNEL     0
-#define PAGE_SIZE_4KB   0
-#define PAGE_SIZE_4MB   1
+#include "../common.h"
+#include "kheap.h"
 
 typedef struct page
 {
-   unsigned int present    : 1;   // Page present in memory
-   unsigned int rw         : 1;   // Read-only if clear, readwrite if set
-   unsigned int user       : 1;   // Supervisor level only if clear
-   unsigned int accessed   : 1;   // Has the page been accessed since last refresh?
-   unsigned int dirty      : 1;   // Has the page been written to since last refresh?
-   unsigned int unused     : 7;   // Amalgamation of unused and reserved bits
-   unsigned int frame      : 20;  // Frame address (shifted right 12 bits)
+    u32int present    : 1;   // Page present in memory
+    u32int rw         : 1;   // Read-only if clear, readwrite if set
+    u32int user       : 1;   // Supervisor level only if clear
+    u32int accessed   : 1;   // Has the page been accessed since last refresh?
+    u32int dirty      : 1;   // Has the page been written to since last refresh?
+    u32int unused     : 7;   // Amalgamation of unused and reserved bits
+    u32int frame      : 20;  // Frame address (shifted right 12 bits)
 } page_t;
 
 typedef struct page_table
 {
-   page_t pages[1024] __attribute__((aligned(4096)));
+    page_t pages[1024];
 } page_table_t;
 
 typedef struct page_directory
 {
-   /**
-      Array of pointers to pagetables.
-   **/
-   page_table_t *tables[1024];
-   /**
-      Array of pointers to the pagetables above, but gives their *physical*
-      location, for loading into the CR3 register.
-   **/
-   unsigned int tablesPhysical[1024];
-   /**
-      The physical address of tablesPhysical. This comes into play
-      when we get our kernel heap allocated and the directory
-      may be in a different location in virtual memory.
-   **/
-   unsigned int physicalAddr;
+    /**
+       Array of pointers to pagetables.
+    **/
+    page_table_t *tables[1024];
+    /**
+       Array of pointers to the pagetables above, but gives their *physical*
+       location, for loading into the CR3 register.
+    **/
+    u32int tablesPhysical[1024];
+
+    /**
+       The physical address of tablesPhysical. This comes into play
+       when we get our kernel heap allocated and the directory
+       may be in a different location in virtual memory.
+    **/
+    u32int physicalAddr;
 } page_directory_t;
 
-
+/**
+   Sets up the environment, page directories etc and
+   enables paging.
+**/
+void initialise_paging();
 
 /**
-  Sets up the environment, page directories etc and
-  enables paging.
+   Causes the specified page directory to be loaded into the
+   CR3 register.
 **/
-void init_paging();
+void switch_page_directory(page_directory_t *new);
 
+/**
+   Retrieves a pointer to the page required.
+   If make == 1, if the page-table in which this page should
+   reside isn't created, create it!
+**/
+page_t *get_page(u32int address, int make, page_directory_t *dir)
+{
+    // Turn the address into an index.
+    address /= 0x1000;
+    // Find the page table containing this address.
+    u32int table_idx = address / 1024;
+    if (dir->tables[table_idx]) // If this table is already assigned
+    {
+        return &dir->tables[table_idx]->pages[address%1024];
+    }
+    else if(make)
+    {
+        u32int tmp;
+        dir->tables[table_idx] = (page_table_t*)kmalloc_ap(sizeof(page_table_t), &tmp);
+        dir->tablesPhysical[table_idx] = tmp | 0x7; // PRESENT, RW, US.
+        return &dir->tables[table_idx]->pages[address%1024];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/**
+   Handler for page faults.
+**/
 void page_fault();
+
+// The kernel's page directory
+page_directory_t *kernel_directory=0;
+
+// The current page directory;
+page_directory_t *current_directory=0;
+
+// A bitset of frames - used or free.
+u32int *frames;
+u32int nframes;
+
+// Defined in kheap.c
+extern u32int placement_address;
+
+// Macros used in the bitset algorithms.
+#define INDEX_FROM_BIT(a) (a/(8*4))
+#define OFFSET_FROM_BIT(a) (a%(8*4))
+
+// Static function to set a bit in the frames bitset
+static void set_frame(u32int frame_addr)
+{
+    u32int frame = frame_addr/0x1000;
+    u32int idx = INDEX_FROM_BIT(frame);
+    u32int off = OFFSET_FROM_BIT(frame);
+    frames[idx] |= (0x1 << off);
+}
+
+// Static function to clear a bit in the frames bitset
+static void clear_frame(u32int frame_addr)
+{
+    u32int frame = frame_addr/0x1000;
+    u32int idx = INDEX_FROM_BIT(frame);
+    u32int off = OFFSET_FROM_BIT(frame);
+    frames[idx] &= ~(0x1 << off);
+}
+
+// Static function to find the first free frame.
+static u32int first_frame()
+{
+    u32int i, j;
+    for (i = 0; i < INDEX_FROM_BIT(nframes); i++)
+    {
+        if (frames[i] != 0xFFFFFFFF) // nothing free, exit early.
+        {
+            // at least one bit is free here.
+            for (j = 0; j < 32; j++)
+            {
+                u32int toTest = 0x1 << j;
+                if ( !(frames[i]&toTest) )
+                {
+                    return i*4*8+j;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+// Function to allocate a frame.
+void alloc_frame(page_t *page, int is_kernel, int is_writeable)
+{
+    if (page->frame != 0)
+    {
+        return;
+    }
+    else
+    {
+        u32int idx = first_frame();
+        if (idx == (u32int)-1)
+        {
+            // PANIC! no free frames!!
+        }
+        set_frame(idx*0x1000);
+        page->present = 1;
+        page->rw = (is_writeable)?1:0;
+        page->user = (is_kernel)?0:1;
+        page->frame = idx;
+    }
+}
+
+// Function to deallocate a frame.
+void free_frame(page_t *page)
+{
+    u32int frame;
+    if (!(frame=page->frame))
+    {
+        return;
+    }
+    else
+    {
+        clear_frame(frame);
+        page->frame = 0x0;
+    }
+}
+
+
+
+void initialise_paging()
+{
+    // The size of physical memory. For the moment we 
+    // assume it is 16MB big.
+    u32int mem_end_page = 0x1000000;
+    
+    nframes = mem_end_page / 0x1000;
+    frames = (u32int*)kmalloc(INDEX_FROM_BIT(nframes));
+    memset(frames, 0, INDEX_FROM_BIT(nframes));
+    
+    // Let's make a page directory.
+    kernel_directory = (page_directory_t*)kmalloc_a(sizeof(page_directory_t));
+    current_directory = kernel_directory;
+
+    // We need to identity map (phys addr = virt addr) from
+    // 0x0 to the end of used memory, so we can access this
+    // transparently, as if paging wasn't enabled.
+    // NOTE that we use a while loop here deliberately.
+    // inside the loop body we actually change placement_address
+    // by calling kmalloc(). A while loop causes this to be
+    // computed on-the-fly rather than once at the start.
+    unsigned int i = 0;
+    while (i < placement_address)
+    {
+        // Kernel code is readable but not writeable from userspace.
+        alloc_frame(get_page(i, 1, kernel_directory), 0, 0);
+        i += 0x1000;
+    }
+
+    // Now, enable paging!
+    switch_page_directory(kernel_directory);
+}
+
+void switch_page_directory(page_directory_t *dir)
+{
+    current_directory = dir;
+    asm volatile("mov %0, %%cr3":: "r"(&dir->tablesPhysical));
+    u32int cr0;
+    asm volatile("mov %%cr0, %0": "=r"(cr0));
+    cr0 |= 0x80000000; // Enable paging!
+    asm volatile("mov %0, %%cr0":: "r"(cr0));
+}
+
+
+void page_fault()
+{
+	// A page fault has occurred.
+	char buffer[]= "Page Fault";
+	serial_write(SERIAL_COM1_BASE,buffer,sizeof(buffer));
+	fb_write(buffer,sizeof(buffer));
+
+}
 
 #endif
